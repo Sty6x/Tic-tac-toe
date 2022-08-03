046bcf14777ac7d4c587b5fa666fd29aaea29595
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-08-03 09:41:54 +0400

    each cell has unique identifier that correlates with gameobject array

--- scripts.js
+++ scripts.js
@@ -4,29 +4,29 @@ console.log('heyho')
 let GameBoardModule = (() => {
     let gameBoardContainer = document.getElementById('game-board-container')
 
-    const GAMEBOARD = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-    ]
+    let GAMEBOARD = [];
 
     function createCells() {
-        for (let row = 0; row < GAMEBOARD.length; row++) {
-            for (let col = 0; col < GAMEBOARD[row].length; col++) {
+        for (let row = 0; row < 3; row++) { 
+            //makes a 2d array  
+            GAMEBOARD.push([row])
+            //removes the first element that includes the row elemenet
+            GAMEBOARD[row].shift(0)
+            for (let col = 0; col < 3; col++) {
+                //appends each iteration of col in row array
+                GAMEBOARD[row].push(col)
                 var cell = document.createElement('div')
                 gameBoardContainer.appendChild(cell)
                 cell.classList.add('cell')
                 // [0][0] [0][1] [0][2]
-                cell.innerHTML = GAMEBOARD[row][col]
-                cell.setAttribute('id', GAMEBOARD[row] + GAMEBOARD[cell])
-
+                cell.setAttribute('id', `${row}${col}`)
             }
         }
     }
     return {
         gameBoardContainer,
         createCells,
-        // GAMEBOARD
+        GAMEBOARD
     }
 })()
 
@@ -36,11 +36,23 @@ function player(pick) {
     let GameBoard = GameBoardModule
     // let cells = GameBoard.createCells()
     let GameboardContainer = GameBoard.gameBoardContainer;
-    // let GameBoardArray = GameBoard.GAMEBOARD;
+    let GameBoardArray = GameBoard.GAMEBOARD;
 
     function aI() {
-        console.log('time')
         // console.log(GameBoardArray)
+        //randomizer set to ID and set innerHTML
+        let IdRandomizer = {
+            IdNum:3,
+            randomNum (){
+                return Math.floor(Math.random()*this.IdNum)
+            }
+        }
+        let firstNum = IdRandomizer.randomNum()
+        let secondNum = IdRandomizer.randomNum()
+        // let firstNum secondNum = Math.floor(Math.random() * 3)
+        console.log(firstNum+secondNum)
+        
+        
     }
 
     function getCellPosition() {
@@ -48,8 +60,8 @@ function player(pick) {
             let target = e.target
             if (!target.matches('.cell')) return
             e.target.innerHTML = pick
-            // GameBoardArray
-            console.log(target.id )
+            console.log(GameBoardArray)
+            console.log(target)
             setTimeout(aI, 500)
         })
     }
