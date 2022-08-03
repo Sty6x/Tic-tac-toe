bb7a1ce25ccdd0ceb9029d50656010f50c38b80b
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-08-03 05:55:00 +0400

    game board is shown when invoked and player is able to return each cell of the game board module

--- scripts.js
+++ scripts.js
@@ -3,29 +3,48 @@ console.log('heyho')
 
 let GameBoardModule = (() => {
     let gameBoardContainer = document.getElementById('game-board-container')
+
     const GAMEBOARD = [
         ['X', 'o', '0'],
         ['o', 'x', 'o'],
         ['x', 'o', 'x']
     ]
 
-    function createCells(text,index) {
+    function createCells() {
         for (let row = 0; row < GAMEBOARD.length; row++) {
             for (let col = 0; col < GAMEBOARD[row].length; col++) {
                 var cell = document.createElement('div')
                 gameBoardContainer.appendChild(cell)
-                cell.classList.add('cells')
-                cell.innerHTML = text                
-                // GAMEBOARD[col][row]
-                console.log(cell)
+                cell.classList.add('cell')
+                // [0][0] [0][1] [0][2]
+                cell.innerHTML = GAMEBOARD[row][col]
             }
         }
-        return cell;
+        return {
+            cell
+        };
     }
-
     return {
+        gameBoardContainer,
         createCells
     }
 })()
 
-GameBoardModule.createCells('Heh')
+
+
+function player() {
+    let cells = GameBoardModule.createCells()
+    let GameboardContainer = GameBoardModule.gameBoardContainer;
+
+    function getCellPosition() {
+        GameboardContainer.addEventListener('click', (e) => {
+            console.log(e.target)
+        })
+    }
+    return {
+        getCellPosition
+    }
+}
+
+let player1 = player()
+player1.getCellPosition()
